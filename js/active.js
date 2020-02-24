2ab97cc897cc2e0919c51a68c0f73e3c4ad92431
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-02-24 18:47:48 +0530

    Complete HomePage

---
+++ js/active.js
@@ -0,0 +1,90 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+    // :: 1.0 Preloader Active Code
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
+
+    // :: 2.0 Nav Active Code
+    if ($.fn.classyNav) {
+        $('#venturaNav').classyNav();
+    }
+
+    // :: 3.0 Sticky Active Code
+    if ($.fn.sticky) {
+        $("#sticker").sticky({
+            topSpacing: 0
+        });
+    }
+
+    // :: 5.0 Sliders Active Code
+    if ($.fn.owlCarousel) {
+        var welcomeSlide = $('.img-slides');
+
+        welcomeSlide.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: true,
+            navText: ['Prev Quest', 'Next Quest'],
+            dots: false,
+            autoplay: true,
+            autoplayTimeout: 7000,
+            smartSpeed: 1000
+        });
+
+        welcomeSlide.on('translate.owl.carousel', function () {
+            var slideLayer = $("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        welcomeSlide.on('translated.owl.carousel', function () {
+            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+    }
+
+    // :: 6.0 ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        browserWindow.scrollUp({
+            scrollSpeed: 900,
+            scrollText: '<i class="fa fa-angle-up"></i>'
+        });
+    }
+
+    // :: 7.0 Tooltip Active Code
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+
+    // :: 8.0 Prevent Default a Click
+    $('a[href="#"]').on('click', function ($) {
+        $.preventDefault();
+    });
+
+    // :: 9.0 Wow Active Code
+    if (browserWindow.width() > 767) {
+        new WOW().init();
+    }
+})(jQuery);
