915b6189fb39d0ebb8b1a11f01db6817436c511c
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-02-25 18:25:18 +0530

    Updated Files

--- index.php
+++ index.php
@@ -179,6 +179,7 @@
   <!-- ##### Hero Area End ##### -->
 
   <!-- ##### About Area Start ##### -->
+  
   <div class="about-area about-area section-padding-50 bg-pattern2">
     <!-- Main about Area -->
     <div class="main-about-area section-padding-100-0">
@@ -293,39 +294,6 @@
     </div>
   </section>
   <!-- ##### Contribution Area End ##### -->
-
-  <!-- ##### Footer Area Start ##### -->
-
-  <!-- Copywrite Area -->
-  <div class="about-area">
-    <div class="copywrite-content fadeInUp">
-      <div class="container h-100">
-        <div class="row h-100 align-items-center">
-          <div class="col-12 col-sm-5">
-            <!-- Copywrite Text -->
-            <p class="copywrite-text" style="color: #666666;">
-                Copyright &copy; <script>
-                  document.write(new Date().getFullYear());
-                </script> VenturaHunt 3.0. All Rights Reserved.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- ##### Footer Area End ##### -->
-
-  <!-- ##### All Javascript Script ##### -->
-  <!-- jQuery-2.2.4 js -->
-  <script src="js/jquery/jquery-2.2.4.min.js"></script>
-  <!-- Popper js -->
-  <script src="js/bootstrap/popper.min.js"></script>
-  <!-- Bootstrap js -->
-  <script src="js/bootstrap/bootstrap.min.js"></script>
-  <!-- All Plugins js -->
-  <script src="js/plugins/plugins.js"></script>
-  <!-- Active js -->
-  <script src="js/active.js"></script>
-</body>
-
-</html>
+  <?php
+  include 'includes/footer.php';
+  ?>
