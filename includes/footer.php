b69c398446cf95b0e4e1097965bed578de02520d
Roshan Kanwar <roshan.18je0696@am.iitism.ac.in>
2020-02-28 12:36:37 +0530

    arena design ready

--- includes/footer.php
+++ includes/footer.php
@@ -1,34 +1,45 @@
-  <!-- ##### Footer Area Start ##### -->
+<!-- ##### Footer Area Start ##### -->
 
-  <!-- Copywrite Area -->
-  <div class="about-area">
-    <div class="copywrite-content fadeInUp">
-      <div class="container h-100">
-        <div class="row h-100 align-items-center">
-          <div class="col-12 col-sm-5">
-            <!-- Copywrite Text -->
-            <p class="copywrite-text" style="color: #666666;">
-                Copyright &copy; <script>
-                  document.write(new Date().getFullYear());
-                </script> VenturaHunt 3.0. All Rights Reserved.
-            </p>
-          </div>
+<!-- Copywrite Area -->
+<div class="about-area">
+  <div class="copywrite-content fadeInUp">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 col-sm-5">
+          <!-- Copywrite Text -->
+          <p class="copywrite-text" style="color: #666666;">
+              Copyright &copy; 2020
+              VenturaHunt 3.0. All Rights Reserved.
+          </p>
         </div>
       </div>
     </div>
   </div>
-  <!-- ##### Footer Area End ##### -->
-  <!-- ##### All Javascript Script ##### -->
-  <!-- jQuery-2.2.4 js -->
-  <script src="js/jquery/jquery-2.2.4.min.js"></script>
-  <!-- Popper js -->
-  <script src="js/bootstrap/popper.min.js"></script>
-  <!-- Bootstrap js -->
-  <script src="js/bootstrap/bootstrap.min.js"></script>
-  <!-- All Plugins js -->
-  <script src="js/plugins/plugins.js"></script>
-  <!-- Active js -->
-  <script src="js/active.js"></script>
-  </body>
+</div>
+<!-- ##### Footer Area End ##### -->
+<!-- ##### All Javascript Script ##### -->
+<!-- jQuery-2.2.4 js -->
+<script src="js/jquery/jquery-2.2.4.min.js"></script>
+<!-- Popper js -->
+<script src="js/bootstrap/popper.min.js"></script>
+<!-- Bootstrap js -->
+<script src="js/bootstrap/bootstrap.min.js"></script>
+<!-- All Plugins js -->
+<script src="js/plugins/plugins.js"></script>
+<!-- Active js -->
+<script src="js/active.js"></script>
+<!-- Typed js -->
+<script src="js/plugins/typed.min.js"></script>
 
-  </html>
+<script type="text/javascript">
+var typed = new Typed('.appearHead', {
+strings: ["Welcome to the Ventura Hunt Arena!", "Hope You're ready to begin your quest."],
+typeSpeed: 50,
+backSpeed: 50,
+loop:true
+});
+</script>
+
+</body>
+
+</html>
